9eee34e0a90844524d8bd58e05f0cbefc3dfc3c6
Danessa Aveiro <danessaveiro@gmail.com>
2023-06-28 20:14:34 -0300

    exemplos da aula 2

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+//conversão explícita
+
+// função Number() => converte string em number
+// função String() => converte number em string
+
+console.log(numero + Number(numeroString))
