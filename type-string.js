4af4f0f84043dbebeab2a90fb3651f5bb64816fa
Danessa Aveiro <danessaveiro@gmail.com>
2023-06-28 16:55:17 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567"; 
+
+const citacao = 'O Leo disse "oi"!';
+console.log(citacao)
+
+// contatenação (+)
+
+const frase = "Meu nome é ";
+const meuNome = "Leonardo";
+
+console.log(frase + meuNome)
+
+// template string OU template literal
