88225fb1310d879d56a0f1324b4b318bd3408b25
Danessa Aveiro <danessaveiro@gmail.com>
2023-06-29 17:01:32 -0300

    exemplos aula 4

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Danessa";
+const idade = 2023-1987;
+const cidade = "Uberaba";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidade;
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci na cidade de ${cidade}.`
+
+console.log(apresentacao)
---
+++ ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+/*
+if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja")
+} else {
+    console.log("Suco")
+}
+*/
+
+            //condição                    //true      //false
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco")
