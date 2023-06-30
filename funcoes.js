7f5cefcd92421c943588217ed55f22f8b8966b8b
Danessa Aveiro <danessaveiro@gmail.com>
2023-06-30 12:15:27 -0300

    exemplos aula 5

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+// let x = "";
+// console.log(x);
+// x = "Oi"
+
+// 1) declara a função
+
+function imprimeTexto (texto) {
+    console.log(texto);
+}
+
+// 2) executa a função (1 ou + vezes)
+
+/*
+imprimeTexto("Oi")
+imprimeTexto("Outro Texto")
+*/
+
+function soma() {
+    return 2 + 2;
+}
+
+//console.log(soma())
+
+imprimeTexto(soma())
---
+++ parametros.js
@@ -0,0 +1,21 @@
+// parâmetros da função
+              //6      //6
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+//console.log(soma(6, 6))
+
+//ordem dos parâmetros
+
+function nomeIdade (nome, idade) {
+    return `Meu nome é ${nome} e eu tenho ${idade} anos.`
+}
+
+//console.log(nomeIdade("Danessa", 36))
+
+function multiplica (numero1, numero2) {
+    return numero1 * numero2
+}
+                            //8         //17
+console.log(multiplica(soma(6, 2), soma(8, 9)))
